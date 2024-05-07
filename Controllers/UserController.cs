0be2da39515fd967e9748a530d078424f40ff4f0
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-05-07 13:48:58 -0300

    Criando o primeiro endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MyFirstApi.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var response = new Response
+        {
+            Age = 10,
+            Name = "Tester"
+        };
+
+        return Ok(response);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
