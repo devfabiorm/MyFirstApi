cd48a785db53f02e217ab6e83a599c5b7881ff11
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-05-07 14:09:00 -0300

    Passando parando parametros como route params

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,12 +7,14 @@ namespace MyFirstApi.Controllers;
 public class UserController : ControllerBase
 {
     [HttpGet]
+    [Route("{id}")]
     [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-    public IActionResult Get(int name)
+    public IActionResult Get(int id)
     {
         var response = new Response
         {
+            Id = 1,
             Age = 10,
             Name = "Tester"
         };
--- Response.cs
+++ Response.cs
@@ -2,6 +2,7 @@
 
 public class Response
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
 }
